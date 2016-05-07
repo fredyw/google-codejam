88ed6677518863b9ef6fd510e02e94034b9b33d2
Fredy Wijaya <fredy.wijaya@gmail.com>
2016-05-07 14:05:38 -0500

    Google Code Jam 2016 - Round 1B
    Initial attempt for close match problem

--- src/main/java/gcj2016/CloseMatch.java
+++ src/main/java/gcj2016/CloseMatch.java
@@ -9,16 +9,83 @@ import java.io.FileReader;
  */
 public class CloseMatch {
     public static void main(String[] args) throws Exception {
+        // File file = new File("src/main/resources/2016/closematch/sample.txt");
         File file = new File("src/main/resources/2016/closematch/B-small-practice.in");
-        // File file = new File("src/main/resources/2016/closematch/B-small-practice.in");
+        // File file = new File("src/main/resources/2016/closematch/B-large-practice.in");
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             int testCases = Integer.parseInt(br.readLine());
             for (int t = 1; t <= testCases; t++) {
-                String c = "";
-                String j = "";
-                // TODO
-                System.out.println(String.format("Case #%d: %s %s", t, c, j));
+                String[] line = br.readLine().split("\\s+");
+                String coder = line[0];
+                String jammer = line[1];
+                String closeMatch = closeMatch(coder, jammer);
+                System.out.println(String.format("Case #%d: %s", t, closeMatch));
             }
         }
+    }
+
+    private enum Score {
+        CODER_JAMMER_EQUAL,
+        CODER_GREATER,
+        JAMMER_GREATER
+    }
+
+    private static String closeMatch(String coder, String jammer) {
+        StringBuilder coderScore = new StringBuilder();
+        StringBuilder jammerScore = new StringBuilder();
+        Score score = Score.CODER_JAMMER_EQUAL;
+        for (int i = 0; i < coder.length(); i++) {
+            char c = coder.charAt(i);
+            char j = jammer.charAt(i);
+            if (score == Score.CODER_JAMMER_EQUAL) {
+                if (c == '?' && j == '?') {
+                    coderScore.append('0');
+                    jammerScore.append('0');
+                } else if (c == '?' && j != '?') {
+                    coderScore.append(j);
+                    jammerScore.append(j);
+                } else if (c != '?' && j == '?') {
+                    coderScore.append(c);
+                    jammerScore.append(c);
+                } else {
+                    if (c < j) {
+                        score = Score.JAMMER_GREATER;
+                    } else if (c > j) {
+                        score = Score.CODER_GREATER;
+                    }
+                    coderScore.append(c);
+                    jammerScore.append(j);
+                }
+            } else if (score == Score.CODER_GREATER) {
+                if (c == '?' && j == '?') {
+                    coderScore.append('0');
+                    jammerScore.append('9');
+                } else if (c == '?' && j != '?') {
+                    coderScore.append('0');
+                    jammerScore.append(j);
+                } else if (c != '?' && j == '?') {
+                    coderScore.append(c);
+                    jammerScore.append('9');
+                } else {
+                    coderScore.append(c);
+                    jammerScore.append(j);
+                }
+            } else if (score == Score.JAMMER_GREATER) {
+                if (c == '?' && j == '?') {
+                    coderScore.append('9');
+                    jammerScore.append('0');
+                } else if (c == '?' && j != '?') {
+                    coderScore.append('9');
+                    jammerScore.append(j);
+                } else if (c != '?' && j == '?') {
+                    coderScore.append(c);
+                    jammerScore.append('0');
+                } else {
+                    coderScore.append(c);
+                    jammerScore.append(j);
+                }
+            }
+        }
+        return coderScore + " " + jammerScore;
     }
 }
